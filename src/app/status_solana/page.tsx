4fbd5afa0436fddb1fe271212053afb95a80a8f7
takuyabe <takuya.abe.io@gmail.com>
2024-04-07 16:53:48 +0900

    add header

--- src/app/status_solana/page.tsx
+++ src/app/status_solana/page.tsx
@@ -4,8 +4,6 @@ import Image from "next/image"
 import { bigint2Float, getRewardState } from "@/lib/reward/reward";
 
 
-
-
 export default async function Page() {
     // const { records, balance } = await getRewardState();
     // const earnSum_ = records
@@ -20,9 +18,26 @@ export default async function Page() {
     // const withdrawSum = bigint2Float(withdrawSum_)
 
     return (<>
-      <Image src="/SleepinWhite.svg" alt="sleepin white" className="m-5" width={100} height={100} />
-      <h3 className="text-center">{`Balance: 10 SOL`}</h3>
-      {/* <Token records={records} earnSum={earnSum} withdrawSum={withdrawSum} /> */}
-
+      <header className="bg-black relative lg:w-full">
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+          <div className="flex">
+            <a href="/" className="">
+              <Image
+                className="relative dark:invert"
+                src="/chro-plus_logo_wht.svg"
+                alt="chro-plus Logo"
+                width={90}
+                height={22}
+                priority
+              />
+            </a>
+          </div>
+          <div className="flex text-white">User.</div>
+        </nav>
+      </header>
+      <div className="m-5">
+        <h3 className="text-center">{`Balance: 10 SOL`}</h3>
+        {/* <Token records={records} earnSum={earnSum} withdrawSum={withdrawSum} /> */}
+      </div>
     </>);
 }
