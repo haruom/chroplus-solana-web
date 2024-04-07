e13658afdf79e955c666fd5057798b9a4375404f
takuyabe <takuya.abe.io@gmail.com>
2024-04-07 16:38:52 +0900

    add header logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,23 @@ import { redirect } from "next/navigation";
 export default function Home() {
   
   return (<>
+
     <header className="bg-black fixed lg:w-full">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1"> </div>
-        <div className="flex"> </div>
+        <div className="flex">
+          <Image
+            className="relative dark:invert"
+            src="/chro-plus_logo_wht.svg"
+            alt="chro-plus Logo"
+            width={90}
+            height={27}
+            priority
+          />
+        </div>
+        <div className="flex">items</div>
       </nav>
     </header>
+
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -42,7 +53,7 @@ export default function Home() {
 
       <div className="relative flex place-items-center">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="relative dark:invert"
           src="/chro-plus_logo_blk.svg"
           alt="chro-plus Logo"
           width={708}
