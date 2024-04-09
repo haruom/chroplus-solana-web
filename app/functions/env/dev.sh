6ecf710c6ef5dd8ae66bbde45aff5b69b34e583e
Katsuhito Matsushima <matwumatwu@gmail.com>
2024-04-09 15:48:03 +0900

    Remove environmental files.

--- app/functions/env/dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-firebase functions:config:set \
-chroplus.wallet.privatekey="5Qfh2asXi84rS2zUTU2XWSsPrHi1U54cxpjdAihAS62Ffbrf1BLq2dmEMCB6mZajNeZaz3jSVm455uJzVQLksmyB" \
-chroplus.arweave.wallet.path="env/arweaveWalletKey.json"
